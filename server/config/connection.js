95b3344b61526b25cf53dcbd32a0038e9af35c7e
Malcolm Franklin <mac.mac5@yahoo.com>
2024-07-30 18:39:33 -0600

    connection js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks' {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Could not connect to MongoDB', err);
+});
 
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
-  
-  module.exports = mongoose.connection;
+module.exports = mongoose.connection;
